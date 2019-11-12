a7e751d4149849c63e4178d7c48c3dcb9aeac844
FDC00073 <FDC00073@IC0-W-N09161R.ssg.global>
2019-11-12 13:23:49 +0900

    채널 통계 추이

--- routes/channel.js
+++ routes/channel.js
@@ -65,22 +65,55 @@ router.post("/count", function(req, res){
 
 router.post("/statistics", function(req, res){
     console.log("Router for IF_DMA_00302");
-
     let size = req.body.size || 10;
     var body = common.getBody(req.body.start_dt, req.body.end_dt, size);
     var index = common.getIndex(req.body.channel);
-    
+    var interval = req.body.interval || "1D";
     if( req.body.category1 !== undefined)
         body.query.bool.filter.push({ term : { category1 : req.body.category1 }});
     if( req.body.category2 !== undefined)
         body.query.bool.filter.push({ term : { category2 : req.body.category2 }});
-
+    body.aggs.day = {
+      	date_histogram : {
+      		field : "startTime",
+      		interval : interval,
+            min_doc_count : 1
+        },
+        aggs : {
+        	index : {
+        		terms : {
+        			field : "_index"
+        		}
+        	}
+        }
+    };
+    
+    var index = common.getIndex(req.body.channel);
+    
     client.search({
-        index ,
+        index,
         body 
     }).then(function(resp){
-        result = [];
-        res.send(resp);
+    	var result = common.getResult( "10", "OK", "channel_count");
+    	var total = 0;
+    	result.data.count = 0;
+        result.data.result = [];
+        test = Object.entries(resp.aggregations.day.buckets);
+        for(i in test){
+        	total = total + test[i][1].doc_count;
+        	test2 = Object.entries(test[i][1].index.buckets);
+        	for(j in test2){
+        		var obj = {
+        			no : j,
+                    channel : test2[j][1].key,
+                    count : test2[j][1].doc_count,
+                    rate : Math.round((test2[j][1].doc_count/resp.hits.total)*100)
+                }
+                result.data.result.push(obj);
+        	}
+        }
+        result.data.count = resp.hits.total;
+        res.send(result);
     }, function(err){
         console.log(err);
     });
