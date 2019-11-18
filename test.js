a49f811724072a5d1338299c1ab957ba5cf90cac
hongseok5 <ohho28@gmail.com>
2019-11-18 17:22:10 +0900

    keyword_extract

--- test.js
+++ test.js
@@ -3,111 +3,63 @@ var dateFormat = require('dateformat');
 var now = dateFormat(new Date(), "yyyymmddHHMMss");
 var hour_ago = new Date().getHours() - 1 ;
 var flatten = require('flat');
+const rp = require('request-promise');
+const request = require('request');
+const elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({ 
+  host : '10.253.42.185:9200',
+  log: 'trace',
+  apiVersion: '6.8' // insert 수행시 필요한 설정
+});
+var option = {
+    uri : 'http://10.253.42.185:12800/txt_to_kwd',
+    method : "POST",
+    body : {
+        mode : "kma",
+        t_vec : "mobile_test",
+        text : null
+    },
+    json : true
+}
+data_set = [];
+data_obj = {
 
-var obj_flatted = {
-    "Mcate" : {
-      "type" : "keyword"
-    },
-    "McateNm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "Pcate" : {
-      "type" : "keyword"
-    },
-    "PcateNm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "age" : {
-      "type" : "long"
-    },
-    "agentId" : {
-      "type" : "keyword"
-    },
-    "agentNm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "category1" : {
-      "type" : "keyword"
-    },
-    "category1Nm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "category2" : {
-      "type" : "keyword"
-    },
-    "category2Nm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "company" : {
-      "type" : "keyword"
-    },
-    "companyNm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "gender" : {
-      "type" : "keyword"
-    },
-    "ifId" : {
-      "type" : "keyword"
-    },
-    "inCate" : {
-      "type" : "keyword"
-    },
-    "inCateNm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "mdId" : {
-      "type" : "keyword"
-    },
-    "mdNm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "productCode" : {
-      "type" : "keyword"
-    },
-    "productNm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "reasonCate1" : {
-      "type" : "keyword"
-    },
-    "reasonCate1Nm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "reasonCate2" : {
-      "type" : "keyword"
-    },
-    "reasonCate2Nm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "vdn" : {
-      "type" : "keyword"
-    },
-    "vdnGrp" : {
-      "type" : "keyword"
-    },
-    "vdnGrpNm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "vdnNm" : {
-      "type" : "keyword",
-      "ignore_above" : 256
-    },
-    "caseId" : {
-      "type" : "keyword"
-    }
 };
-var test = Object.entries(obj_flatted)
-console.log(test);
+
+client.search({
+    index : "stt_201911",
+    body : {
+        size : 1
+    }
+}).then(function(resp){
+    console.log("test: " + resp.hits.hits)
+    for( i in resp.hits.hits){
+
+        option.body.text =  resp.hits.hits[i]._source.source.timeNtalk;
+
+        rp(option).then(function(data){
+            var document = {
+                index : "ssg_stt_test",
+                type : "doc",
+                id : resp.hits.hits[i]._source.source.startTime + resp.hits.hits[i]._source.source.extension,
+                body : {
+                    timeNtalk : resp.hits.hits[i]._source.source.timeNtalk,
+                    startTime : resp.hits.hits[i]._source.source.startTime,
+                    extension : resp.hits.hits[i]._source.source.extension,
+                    keyword_count : data.output
+                }
+            };
+            client.index(document).then(function(resp){
+                console.log("success");
+            }, function(err){
+                console.log(err);
+            })
+        })
+        
+        
+    }
+}), function(err){
+    console.log(err);
+};
+
+
