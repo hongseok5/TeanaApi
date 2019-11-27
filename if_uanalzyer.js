b57f1adfd726ef5a0ae5a58976b3622e59332b24
FDC00073 <FDC00073@IC0-W-N09161R.ssg.global>
2019-11-27 16:46:35 +0900

    수정

--- if_uanalzyer.js
+++ if_uanalzyer.js
@@ -187,91 +187,87 @@ var io = schedule.scheduleJob('30 30 * * * *', function(){
 					if(err) { return callerror(err); }
 					filedata = JSON.parse(filedata);
 					var counsetltypeid = "";
+					var callsetseq = "";
 					pool.getConnection(function(err, connection){
-						var querystring  = "SELECT NCT.COUNSEL_TYPE_ID FROM NX_COUNSEL_TYPE NCT, NX_EMP NE WHERE NCT.DEPT_ID = NE.DEPT_ID AND NE.CTI_ID = ? LIMIT 1";
+						var querystring  = "SELECT NCT.COUNSEL_TYPE_ID, (SELECT CURRENT_VAL FROM NX_SEQUENCE WHERE SEQUENCE_ID = 'CALL_SET_SEQ'  LIMIT 1) AS SEQ FROM NX_COUNSEL_TYPE NCT, NX_EMP NE WHERE NCT.DEPT_ID = NE.DEPT_ID AND NE.CTI_ID = ? LIMIT 1";
 						connection.query(querystring, [ filedata.agentId ], function(err, rows, fields) {
 							if (!err){
 								for(var i=0; i<rows.length;i++){
 							    	counsetltypeid = rows[i].COUNSEL_TYPE_ID; //resultId에 해당하는 부분만 가져옴
+							    	callsetseq = rows[i].SEQ; //resultId에 해당하는 부분만 가져옴
 							    }
-								param = { "id" : counsetltypeid,
-							    		  "text" : filedata.timeNtalk
-							    		};
-							    options1.body = JSON.stringify(param);
-							    rp(options1).then(function ( data ){
-							    	var querystringuacall  = "SELECT CALL_NUM FROM UA_CALL WHERE START_TIME = ? AND EXTENSION = ?";
-							    	var uacallnum = "";
-							    	var callSQL = "CALL call_counsel_set()";
-							    	var inserEstDtltQL = "  INSERT INTO NX_COUNSEL_ITEM_HIS (CALL_NUM, START_TIME, EXTENSION, COUNSEL_TYPE_ID, EMP_ID  "
-											    		+", DEPT_ID, LEV3_COUNSEL_ITEM_ID, LEV4_COUNSEL_ITEM_ID, LEV3_ITEM_POINT, LEV4_ITEM_POINT "
-											    		+", ITEM_COUNT, ITEM_TYPE_CD, REG_ID, REG_IP, REG_DTM, MOD_ID, MOD_IP, MOD_DTM) "
-											    		+"select ?, ?, ?, ?, emp_id, dept_id, ?, ?, ?, ?, ?, ?, 'SYSTEM', '0.0.0.0', sysdate(), 'SYSTEM', '0.0.0.0', sysdate() from NX_EMP where cti_id=? ";
+								if(counsetltypeid != null){
+									param = { "id" : counsetltypeid,
+								    		  "text" : filedata.timeNtalk.replace(/[0-9]/g, "")
+								    		};
+								    options1.body = JSON.stringify(param);
+								    rp(options1).then(function ( data ){
+								    	var callSQL = "CALL call_counsel_set(?, ?, ?, ?)";
+								    	var inserEstDtlHisSQL = "  INSERT INTO NX_COUNSEL_ITEM_HIS (CALL_SET_SEQ, START_TIME, EXTENSION, COUNSEL_TYPE_ID, EMP_ID  "
+												    		+", DEPT_ID, LEV3_COUNSEL_ITEM_ID, LEV4_COUNSEL_ITEM_ID, LEV3_ITEM_POINT, LEV4_ITEM_POINT "
+												    		+", ITEM_COUNT, ITEM_TYPE_CD, REG_ID, REG_IP, REG_DTM, MOD_ID, MOD_IP, MOD_DTM) "
+												    		+"select ?, ?, ?, ?, emp_id, dept_id, ?, ?, ?, ?, ?, ?, 'SYSTEM', '0.0.0.0', sysdate(), 'SYSTEM', '0.0.0.0', sysdate() from NX_EMP where cti_id=? ";
 
-							    	data = JSON.parse(data);
-						    			
-							    	connection.query(querystringuacall, [ filedata.startTime, filedata.extension ], function(err, rows, fields) {
-								    		if (!err){
-												for(var i=0; i<rows.length;i++){
-													uacallnum = rows[i].CALL_NUM; //resultId에 해당하는 부분만 가져옴
-											    }
-												for(i in data.matches){
-										    		var extrarr = data.matches[i].extradata.split(","); 
-										    		var counselitemid4 = extrarr[2].substring(extrarr[2].lastIndexOf("=")+1, extrarr[2].length);
-										    		var counselitemid3 = extrarr[1].substring(extrarr[1].lastIndexOf("=")+1, extrarr[1].length);
-										    		var lev4itempoint = extrarr[0].substring(extrarr[0].lastIndexOf("=")+1, extrarr[0].length);
-										    		var lev3itempoint = extrarr[3].substring(extrarr[3].lastIndexOf("=")+1, extrarr[3].length);
-										    		var itemtypecd = extrarr[4].substring(extrarr[4].lastIndexOf("=")+1, extrarr[4].length);
-										    		var checkrow = data.matches.length-1;
-										    		var inserEstDtlquery = connection.query(inserEstDtltQL, [ uacallnum, filedata.startTime, filedata.extension, data.id, counselitemid3, counselitemid4, lev3itempoint, lev4itempoint, data.matches[i].frequency, itemtypecd, filedata.agentId ], function (err, rows) {
-											    	    if(err){
-											    	        connection.release();
-											    	        throw err;
-											    	    }else{
-											    	    	logger.error("if_uanalzyer_Db_Query_inserEstDtltQL", err);
-														}
-											    	    connection.commit(function(err){
-											    	        if(err){
-											    	            connection.rollback(function(err){
-											    	                throw err;
-											    	            });
-											    	        } else {
-											    	            console.log("Updated successfully!");
-											    	            if(i == checkrow){
-													    	    	var callSQLquery = connection.query(callSQL, function (err, rows) {
-															    	    if(err){
-															    	    	connection.release();
-															    	        throw err;
-															    	    }else{
-															    	    	logger.error("if_uanalzyer_Db_Query_callSQL", err);
-																		}
-															    	    connection.commit(function(err){
-															    	        if(err){
-															    	            connection.rollback(function(err){
-															    	                throw err;
-															    	            });
-															    	        } else {
-															    	            console.log("Updated successfully!");
-															    	        }
-															    	    });
-															    	    fs.unlink(config.backup_path+file, function(err){
-															    	        if( err ) throw err;
-															    	        console.log('file deleted');
-															    	    });
-															    	});
-													    	    }
-											    	        }
-											    	    });
-											    	    
-											    	    connection.release();
-											    	});
-										    	}
-											}else{
-												logger.error("if_uanalzyer_Db_Query_querystringuacall", err);
-											}
-								    	});
-								}).catch(function (err){
-									logger.error("if_uanalzyer_/voc/evaluation/_match", err);
-							    });
+								    	data = JSON.parse(data);
+								    	
+								    	for(i in data.matches){
+								    		var extrarr = data.matches[i].extradata.split(","); 
+								    		var counselitemid4 = extrarr[2].substring(extrarr[2].lastIndexOf("=")+1, extrarr[2].length);
+								    		var counselitemid3 = extrarr[1].substring(extrarr[1].lastIndexOf("=")+1, extrarr[1].length);
+								    		var lev4itempoint = extrarr[0].substring(extrarr[0].lastIndexOf("=")+1, extrarr[0].length);
+								    		var lev3itempoint = extrarr[3].substring(extrarr[3].lastIndexOf("=")+1, extrarr[3].length);
+								    		var itemtypecd = extrarr[4].substring(extrarr[4].lastIndexOf("=")+1, extrarr[4].length);
+								    		var checkrow = data.matches.length-1;
+								    		var inserEstDtlquery = connection.query(inserEstDtlHisSQL, [ callsetseq, filedata.startTime, filedata.extension, data.id, counselitemid3, counselitemid4, lev3itempoint, lev4itempoint, data.matches[i].frequency, itemtypecd, filedata.agentId ], function (err, rows) {
+								    			if(err){
+									    	        connection.release();
+									    	        throw err;
+									    	    }else{
+									    	    	logger.error("if_uanalzyer_Db_Query_inserEstDtltQL", err);
+												}
+									    	    connection.commit(function(err){
+									    	        if(err){
+									    	            connection.rollback(function(err){
+									    	                throw err;
+									    	            });
+									    	        } else {
+									    	            console.log("Updated successfully!");
+									    	            if(i == checkrow){
+									    	            	var callSQLquery = connection.query(callSQL, [ callsetseq, filedata.startTime, filedata.extension, filedata.agentId ], function (err, rows) {
+											    	    		if(err){
+													    	    	console.log("bchm err = "+err);
+													    	    	connection.release();
+													    	        throw err;
+													    	    }else{
+													    	    	logger.error("if_uanalzyer_Db_Query_callSQL", err);
+																}
+													    	    connection.commit(function(err){
+													    	        if(err){
+													    	            connection.rollback(function(err){
+													    	                throw err;
+													    	            });
+													    	        } else {
+													    	            console.log("Updated successfully!");
+													    	        }
+													    	    });
+													    	    fs.unlink(config.backup_path+file, function(err){
+													    	        if( err ) throw err;
+													    	        console.log('file deleted');
+													    	    });
+													    	});
+											    	    }
+									    	        }
+									    	    });
+									    	    
+									    	    connection.release();
+									    	});
+								    	}
+									}).catch(function (err){
+										logger.error("if_uanalzyer_/voc/evaluation/_match", err);
+								    });
+								}else{
+									logger.error("if_uanalzyer_COUNSEL_TYPE_ID_null", err);
+								}
 							}
 							else
 								logger.error("if_uanalzyer_Db_Query_2", err);
