69455d68d97731d2fc696c43cb82f32e66dea057
FDC00073 <FDC00073@IC0-W-N09161R.ssg.global>
2019-12-04 12:34:48 +0900

    파일 업로드 및 대유 인터페이스

--- if_dma_00004.js
+++ if_dma_00004.js
@@ -6,7 +6,20 @@ const fs = require('fs');
 var approot = require('app-root-path');
 var config = require(approot + '/config/config');
 const winstonConfig = require(approot + '/lib/logger');
+var crypto = require(approot + '/lib/crypto');
+const mariadb = require('mysql');
+const conn = {
+    host : '10.253.42.184',
+    user : 'ssgtv',
+    password : 'af6676387824a0ee9bbae73e3da0671e',
+    database : 'ssgtv',
+    connectionLimit : 5
+};
 
+conn.password = crypto.strdecrypto(conn.password);
+
+
+var pool = mariadb.createPool(conn);
 /************************************************************
  * 로그 설정.
  ************************************************************/
@@ -41,30 +54,71 @@ var options2 = {
 
 function getData(){
     var path = config.send_save_path;
-    var sj = schedule.scheduleJob('*/10 * * * * *', function(){
+    var sj = schedule.scheduleJob('30 30 * * * *', function(){
+	    var send_data = {};
+	    send_data.params = [];
+	    !fs.existsSync(config.sent_save_path) && fs.mkdirSync(config.sent_save_path);
+	    var z = 0;
+	    pool.getConnection(function(err, connection){
+	    	fs.readdir(config.send_smry_path, function(err, filelist){
+	        	console.log('bchm filelist.length = '+filelist.length);
+	        	filelist.forEach(function(file) {
+	        		console.log('bchm file = '+file);
+	        		fs.readFile(config.send_smry_path+file , 'utf-8' , function(err , filedata){
+	        			if(err) { return callerror(err); }
+	        			filedata = JSON.parse(filedata);
+	        			var querystring  = "UPDATE UA_CALL SET CATEGORY1 = LPAD(?, 10, '0'), SUMMARY = ? WHERE START_TIME = ? AND EXTENSION = ?";
+	        			console.log('bchm filedata.category = '+filedata.category);
+	        			console.log('bchm filedata.summary = '+filedata.summary);
+	        			console.log('bchm filedata.startTime = '+filedata.startTime);
+	        			console.log('bchm filedata.extension = '+filedata.extension);
+	        			
+	        			var callSQLquery = connection.query(querystring, [ filedata.category, filedata.summary, filedata.startTime, filedata.extension ], function (err, rows) {
+	        				if(err){
+					        	connection.release();
+					            throw err;
+					        }else{
+					        	logger.info("if_dma_00004_Db_Query_callSQLquery", err);
+							}
+					        connection.commit(function(err){
+					            if(err){
+					                connection.rollback(function(err){
+					                    throw err;
+					                });
+					            } else {
+					                console.log("Updated successfully!");
+					            }
+					        });
+					        z = parseInt(z)+1;
+					        console.log('bchm z = '+z);
+					        send_data.sendTime = dateFormat(new Date(), "yyyymmddHHMMss");
+				        	fs.rename(config.send_smry_path+file, config.sent_save_path+send_data.sendTime+z, callback);
+					    });
+					});
+	    		});
+	    		
+	    	});
+	    });
+    });  
+    sj.invoke();
+    var sj2 = schedule.scheduleJob('30 30 * * * *', function(){
     	var send_data = {};
         send_data.params = [];
-
+        !fs.existsSync(config.send_move_path) && fs.mkdirSync(config.send_move_path);
+        var z = 0;
         fs.readdir(config.send_save_path, function(err, filelist){
         	filelist.forEach(function(file) {
         		fs.readFile(config.send_save_path+file , 'utf-8' , function(err , filedata){
+        			if(err) { return callerror(err); }
+        			filedata = JSON.parse(filedata);
         			send_data.params.push(filedata);
-        		});
-        		fs.unlink(config.send_save_path+file, function(err){
-	    	        if( err ) throw err;
-	    	        console.log('file deleted');
-	    	    });
-        	});
-        	
-        });
-        send_data.sendTime = dateFormat(new Date(), "yyyymmddHHMMss");
-        !fs.existsSync(config.sent_save_path+send_data.sendTime) && fs.mkdirSync(config.sent_save_path+send_data.sendTime);
-        var filename = config.sent_save_path+send_data.sendTime;
-    	var filecontext = send_data;
-        fs.writeFile(filename, filecontext, "utf8", function(err) {
-        	logger.info("error file : " + err);
-        });
-    	
+        			z = parseInt(z)+1;
+        			send_data.sendTime = dateFormat(new Date(), "yyyymmddHHMMss");
+        			fs.rename(config.send_save_path+file, config.send_move_path+send_data.sendTime+z, callback);
+				});
+    		});
+    	});
+        
         rp(options1)
         .then(function (body) {
             var token = JSON.parse(body);
@@ -82,8 +136,17 @@ function getData(){
         	logger.error(err);
         });
     });  
-    sj.invoke();
+    sj2.invoke();
 }
 
-//getData();
+function callback(){
+	console.log('file remove!!');
+}
+
+function callerror(err){
+	logger.error("if_dma_00004_Db_file_error", err);
+	console.log('file error!! '+err);
+}
+
+getData();
 module.exports = getData;
