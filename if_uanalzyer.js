aeae844975cc49723631c6278a97382a972bde33
FDC00073 <FDC00073@IC0-W-N09161R.ssg.global>
2020-01-03 14:13:48 +0900

    널처리

--- if_uanalzyer.js
+++ if_uanalzyer.js
@@ -73,135 +73,141 @@ var io = schedule.scheduleJob('0 30 3 * * *', function(){
 						connection.query(querystring, [ filedata.inCate,filedata.direction,filedata.ctiId ], function(err, rows, fields) {
 							console.log('db querystring');
 							if (!err){
-								for(var i=0; i<rows.length;i++){
-							    	counsetltypeid = rows[i].counsel_type_id; //resultId에 해당하는 부분만 가져옴
-							    	callsetseq = rows[i].seq; //resultId에 해당하는 부분만 가져옴
-							    }
-								var updateCnt = parseInt(callsetseq)+1;
-								connection.query(updateSequence, [ updateCnt ], function (err, rows) {
-				    	    		if(err){
-				    	    			logger.error("if_uanalzyer_Db_Query_updateCurrent", err);
-						    	    }else{
-						    	    	logger.info("if_uanalzyer_Db_Query_updateCurrent", err);
-									}
-						    	    connection.commit(function(err){
-						    	        if(err){
-						    	        	connection.rollback(function(err){
-						    	            	logger.error("if_uanalzyer_Db_Query_updateCurrent_rollback", err);
-						    	            });
-						    	        } else {
-						    	            console.log("Updated successfully!");
-						    	        }
-						    	    });
-						    	});
-								if(counsetltypeid != null || counsetltypeid != ""){
-									connection.query(querystring2, [ filedata.startTime,filedata.extension ], function(err, rows, fields) {
-										var context_text = "";
-										for(z in rows){
-											context_text = context_text+"\n\t"+rows[z].talk_content;
+								console.log('bchm = '+rows.length); 
+								if(rows.length == 0){
+									fs.rename(config.backup_path+file, config.backup_path_bak+file, callback);
+				    			 	logger.info("if_uanalzyer_Db_Query_db counsel_type_id_null_"+file);
+								}else{
+									for(var i=0; i<rows.length;i++){
+								    	counsetltypeid = rows[i].counsel_type_id; //resultId에 해당하는 부분만 가져옴
+								    	callsetseq = rows[i].seq; //resultId에 해당하는 부분만 가져옴
+								    }
+									var updateCnt = parseInt(callsetseq)+1;
+									connection.query(updateSequence, [ updateCnt ], function (err, rows) {
+					    	    		if(err){
+					    	    			logger.error("if_uanalzyer_Db_Query_updateCurrent", err);
+							    	    }else{
+							    	    	logger.info("if_uanalzyer_Db_Query_updateCurrent", err);
 										}
-										var filedataset = context_text.replace(/[0-9]/g, "");
-										param = { "id" : counsetltypeid,
-									    		  "text" : filedataset.replace(/ /gi, "")
-									    		};
-												
-										logger.debug("counsetltypeid : " +counsetltypeid);		
-												
-									    options1.body = JSON.stringify(param);
-									    rp(options1).then(function ( data ){
-									    	console.log('db rp(options1)');
-									    	var callSQL = "call call_counsel_set(?, ?, ?, ?)";
-									    	var inserEstDtlHisSQL = "  insert into nx_counsel_item_his (call_set_seq, start_time, extension, counsel_type_id, emp_id  "
-													    		+", dept_id, lev3_counsel_item_id, lev4_counsel_item_id, lev3_item_point, lev4_item_point "
-													    		+", item_count, item_type_cd, reg_id, reg_ip, reg_dtm, mod_id, mod_ip, mod_dtm) "
-													    		+"select ?, ?, ?, ?, emp_id, dept_id, ?, ?, ?, ?, ?, ?, 'SYSTEM', '0.0.0.0', sysdate(), 'SYSTEM', '0.0.0.0', sysdate() from nx_emp where cti_id=? ";
+							    	    connection.commit(function(err){
+							    	        if(err){
+							    	        	connection.rollback(function(err){
+							    	            	logger.error("if_uanalzyer_Db_Query_updateCurrent_rollback", err);
+							    	            });
+							    	        } else {
+							    	            console.log("Updated successfully!");
+							    	        }
+							    	    });
+							    	});
+									if(counsetltypeid != null || counsetltypeid != ""){
+										connection.query(querystring2, [ filedata.startTime,filedata.extension ], function(err, rows, fields) {
+											var context_text = "";
+											for(z in rows){
+												context_text = context_text+"\n\t"+rows[z].talk_content;
+											}
+											var filedataset = context_text.replace(/[0-9]/g, "");
+											param = { "id" : counsetltypeid,
+										    		  "text" : filedataset.replace(/ /gi, "")
+										    		};
+													
+											logger.debug("counsetltypeid : " +counsetltypeid);		
+													
+										    options1.body = JSON.stringify(param);
+										    rp(options1).then(function ( data ){
+										    	console.log('db rp(options1)');
+										    	var callSQL = "call call_counsel_set(?, ?, ?, ?)";
+										    	var inserEstDtlHisSQL = "  insert into nx_counsel_item_his (call_set_seq, start_time, extension, counsel_type_id, emp_id  "
+														    		+", dept_id, lev3_counsel_item_id, lev4_counsel_item_id, lev3_item_point, lev4_item_point "
+														    		+", item_count, item_type_cd, reg_id, reg_ip, reg_dtm, mod_id, mod_ip, mod_dtm) "
+														    		+"select ?, ?, ?, ?, emp_id, dept_id, ?, ?, ?, ?, ?, ?, 'SYSTEM', '0.0.0.0', sysdate(), 'SYSTEM', '0.0.0.0', sysdate() from nx_emp where cti_id=? ";
 
-									    	console.log('db data'+data);
-									    	data = JSON.parse(data);
-									    	console.log('db data.matches'+data.matches.length);
-									    	/*if(data.matches.length == 0){
-									    		var callSQLquery = connection.query(callSQL, [ callsetseq, filedata.startTime, filedata.extension, filedata.ctiId ], function (err, rows) {
-								    	    		if(err){
-								    	    			logger.error("if_uanalzyer_Db_Query_db callSQLquery", err);
-										    	    }else{
-										    	    	logger.info("if_uanalzyer_Db_Query_callSQL bchm1", err);
-													}
-										    	    connection.commit(function(err){
-										    	        if(err){
-										    	            connection.rollback(function(err){
-										    	            	logger.error("if_uanalzyer_Db_Query_db callSQLquery_rollback", err);
-										    	            });
-										    	        } else {
-										    	            console.log("Updated successfully!");
-										    	        }
-										    	    });
-										    	    // bchm 파일 이관
-										    	    //fs.rename(config.backup_path+file, config.backup_path_bak+file, callback);
-										    	});
-									    	}*/
-									    	var checklev3item = "";
-									    	for(i in data.matches){
-									    		console.log('db data.matches');
-									    		var extrarr = data.matches[i].extradata.split(","); 
-									    		var counselitemid4 = extrarr[2].substring(extrarr[2].lastIndexOf("=")+1, extrarr[2].length);
-									    		var counselitemid3 = extrarr[1].substring(extrarr[1].lastIndexOf("=")+1, extrarr[1].length);
-									    		var lev4itempoint = extrarr[0].substring(extrarr[0].lastIndexOf("=")+1, extrarr[0].length);
-									    		var lev3itempoint = extrarr[3].substring(extrarr[3].lastIndexOf("=")+1, extrarr[3].length);
-									    		var itemtypecd = extrarr[4].substring(extrarr[4].lastIndexOf("=")+1, extrarr[4].length);
-									    		var checkrow = data.matches.length-1;
-									    		var inserEstDtlquery = connection.query(inserEstDtlHisSQL, [ callsetseq, filedata.startTime, filedata.extension, data.id, counselitemid3, counselitemid4, lev3itempoint, lev4itempoint, data.matches[i].frequency, itemtypecd, filedata.ctiId ], function (err, rows) {
-									    			console.log('db callsetseq');
-									    			if(err){
-									    				fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
-									    			 	logger.error("if_uanalzyer_Db_Query_db callsetseq", err);
-										    	    }else{
-										    	    	logger.info("if_uanalzyer_Db_Query_inserEstDtltQL", err);
-													}
-									    			if(checklev3item != lev3itempoint){
-									    				checklev3item = lev3itempoint;
-									    				connection.commit(function(err){
+										    	console.log('db data'+data);
+										    	data = JSON.parse(data);
+										    	console.log('db data.matches'+data.matches.length);
+										    	/*if(data.matches.length == 0){
+										    		var callSQLquery = connection.query(callSQL, [ callsetseq, filedata.startTime, filedata.extension, filedata.ctiId ], function (err, rows) {
+									    	    		if(err){
+									    	    			logger.error("if_uanalzyer_Db_Query_db callSQLquery", err);
+											    	    }else{
+											    	    	logger.info("if_uanalzyer_Db_Query_callSQL bchm1", err);
+														}
+											    	    connection.commit(function(err){
 											    	        if(err){
-											    	        	fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
 											    	            connection.rollback(function(err){
-											    	            	logger.error("if_uanalzyer_Db_Query_db callsetseq_rollback", err);
+											    	            	logger.error("if_uanalzyer_Db_Query_db callSQLquery_rollback", err);
 											    	            });
 											    	        } else {
 											    	            console.log("Updated successfully!");
-											    	            if(i == checkrow){
-											    	            	var callSQLquery = connection.query(callSQL, [ callsetseq, filedata.startTime, filedata.extension, filedata.ctiId ], function (err, rows) {
-													    	    		if(err){
-													    	    			fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
-															    	    	logger.error("if_uanalzyer_Db_Query_callSQL", err);
-															    	    }else{
-															    	    	logger.info("if_uanalzyer_Db_Query_callSQL", err);
-																		}
-															    	    connection.commit(function(err){
-															    	        if(err){
-															    	        	fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
-															    	            connection.rollback(function(err){
-															    	            	logger.error("if_uanalzyer_Db_Query_callSQL_rollback", err);
-															    	            });
-															    	        } else {
-															    	            console.log("Updated successfully!");
-															    	        }
-															    	    });
-															    	    //bchm 파일 이관
-															    	    //fs.rename(config.backup_path+file, config.backup_path_bak+file, callback);
-															    	});
-													    	    }
 											    	        }
 											    	    });
-									    			}
-										    	});
-									    	}
-										}).catch(function (err){
-											fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
-											logger.error("if_uanalzyer_/voc/evaluation/_match", err);
-									    });
-									});
-								}else{
-									fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
-									logger.error("if_uanalzyer_COUNSEL_TYPE_ID_null", err);
+											    	    // bchm 파일 이관
+											    	    //fs.rename(config.backup_path+file, config.backup_path_bak+file, callback);
+											    	});
+										    	}*/
+										    	var checklev3item = "";
+										    	for(i in data.matches){
+										    		console.log('db data.matches');
+										    		var extrarr = data.matches[i].extradata.split(","); 
+										    		var counselitemid4 = extrarr[2].substring(extrarr[2].lastIndexOf("=")+1, extrarr[2].length);
+										    		var counselitemid3 = extrarr[1].substring(extrarr[1].lastIndexOf("=")+1, extrarr[1].length);
+										    		var lev4itempoint = extrarr[0].substring(extrarr[0].lastIndexOf("=")+1, extrarr[0].length);
+										    		var lev3itempoint = extrarr[3].substring(extrarr[3].lastIndexOf("=")+1, extrarr[3].length);
+										    		var itemtypecd = extrarr[4].substring(extrarr[4].lastIndexOf("=")+1, extrarr[4].length);
+										    		var checkrow = data.matches.length-1;
+										    		var inserEstDtlquery = connection.query(inserEstDtlHisSQL, [ callsetseq, filedata.startTime, filedata.extension, data.id, counselitemid3, counselitemid4, lev3itempoint, lev4itempoint, data.matches[i].frequency, itemtypecd, filedata.ctiId ], function (err, rows) {
+										    			console.log('db callsetseq');
+										    			if(err){
+										    				fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
+										    			 	logger.error("if_uanalzyer_Db_Query_db callsetseq", err);
+											    	    }else{
+											    	    	logger.info("if_uanalzyer_Db_Query_inserEstDtltQL", err);
+														}
+										    			if(checklev3item != lev3itempoint){
+										    				checklev3item = lev3itempoint;
+										    				connection.commit(function(err){
+												    	        if(err){
+												    	        	fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
+												    	            connection.rollback(function(err){
+												    	            	logger.error("if_uanalzyer_Db_Query_db callsetseq_rollback", err);
+												    	            });
+												    	        } else {
+												    	            console.log("Updated successfully!");
+												    	            if(i == checkrow){
+												    	            	var callSQLquery = connection.query(callSQL, [ callsetseq, filedata.startTime, filedata.extension, filedata.ctiId ], function (err, rows) {
+														    	    		if(err){
+														    	    			fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
+																    	    	logger.error("if_uanalzyer_Db_Query_callSQL", err);
+																    	    }else{
+																    	    	logger.info("if_uanalzyer_Db_Query_callSQL", err);
+																			}
+																    	    connection.commit(function(err){
+																    	        if(err){
+																    	        	fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
+																    	            connection.rollback(function(err){
+																    	            	logger.error("if_uanalzyer_Db_Query_callSQL_rollback", err);
+																    	            });
+																    	        } else {
+																    	            console.log("Updated successfully!");
+																    	        }
+																    	    });
+																    	    //bchm 파일 이관
+																    	    //fs.rename(config.backup_path+file, config.backup_path_bak+file, callback);
+																    	});
+														    	    }
+												    	        }
+												    	    });
+										    			}
+											    	});
+										    	}
+											}).catch(function (err){
+												fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
+												logger.error("if_uanalzyer_/voc/evaluation/_match", err);
+										    });
+										});
+									}else{
+										fs.rename(config.backup_path+file, config.backup_path_error+file, callback);
+										logger.error("if_uanalzyer_COUNSEL_TYPE_ID_null", err);
+									}
 								}
 							}
 							else{
