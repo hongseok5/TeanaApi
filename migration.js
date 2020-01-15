b93d76b57d6fdac32ce6badcced4f940b41e5b26
kiyeop <kiyeop@gmail.com>
2020-01-15 09:12:21 +0900

    Update migration.js
    딜레이 0.5초 주는 것 수정.

--- migration.js
+++ migration.js
@@ -6,25 +6,19 @@ var fs = require('fs');
 var approot = require('app-root-path');
 var config = require(approot + '/config/config');
 const winstonConfig = require(approot + '/lib/logger');
+function sleep(ms){
+	  return new Promise( resolve => {
+	    setTimeout(resolve, ms);
+	  })
+	}
+var ms = 1000;
 /*******************************************************************************
  * 로그 설정.
  ******************************************************************************/
 winston.loggers.add("migration", winstonConfig.createLoggerConfig("migration"));
 var logger = winston.loggers.get("migration");
 
-var options2 = {
-    method: 'POST',
-    uri: 'http://10.253.42.185:3001/receive/call',
-    headers: {
-        "Authorization" : null,
-        "Content-Type" : "application/json",
-    },
-    timeout: 10000,
-    body : ""
-};
-
-
-var io = schedule.scheduleJob('0 30 3 * * *', function(){
+function getData(){
 	logger.info("migration_start");
 	
 	!fs.existsSync(config.migration) && fs.mkdirSync(config.migration);
@@ -33,61 +27,12 @@ var io = schedule.scheduleJob('0 30 3 * * *', function(){
     		fs.readFile(config.migration+file , 'utf-8' , function(err , filedata){
     			if(err) { return callerror(err); }
     			try{
-    				
+
     				filedata = JSON.parse(filedata);
     				console.log('bchm filedata = '+JSON.stringify(filedata)); 
     				
     				for(i in filedata){
-    					var myTimer = setInterval(function(){ 
-    						var document = {
-        	    				    index : 'call_'+filedata[i].ifId.slice(0,6),
-        	    				    type : "doc",
-        	    				    id : filedata[i].ifId.replace('_','-'),
-        	    				    body : { 
-        	    				    	doc : {
-        	    				    		  vdn : filedata[i].vdn,
-        	    							  vdnGrp : filedata[i].vdnGrp,
-        	    							  vdnNm : filedata[i].vdnNm,
-        	    							  vdnGrpNm : filedata[i].vdnGrpNm,
-        	    							  inCate : filedata[i].inCate,
-        	    							  inCateNm : filedata[i].inCateNm,
-        	    							  category1 : filedata[i].category1,
-        	    							  category1Nm : filedata[i].category1Nm,
-        	    							  category2 : filedata[i].category2,
-        	    							  category2Nm : filedata[i].category2Nm,
-        	    							  productCode : filedata[i].productCode,
-        	    							  productNm : filedata[i].productNm,
-        	    							  Mcate : filedata[i].Mcate,
-        	    							  McateNm : filedata[i].McateNm,
-        	    							  company : filedata[i].company,
-        	    							  companyNm : filedata[i].companyNm,
-        	    							  mdId : filedata[i].mdId,
-        	    							  mdNm : filedata[i].mdNm,
-        	    							  PCate : filedata[i].PCate,
-        	    							  PCateNm : filedata[i].PCateNm,
-        	    							  age : filedata[i].age,
-        	    							  gender : filedata[i].gender,
-        	    							  reasonCate1 : filedata[i].reasonCate1,
-        	    							  reasonCate1Nm : filedata[i].reasonCate1Nm,
-        	    							  reasonCate2 : filedata[i].reasonCate2,
-        	    							  reasonCate2Nm : filedata[i].reasonCate2Nm,
-        	    							  caseId : filedata[i].caseId,
-        	    							  caseNumber : filedata[i].caseNumber,
-        	    							  customerNumber : filedata[i].customerNumber,
-        	    							  status : filedata[i].status,
-        	    							  reasonDescription : filedata[i].reasonDescription 
-        	    				    	},
-        	    					doc_as_upsert: true
-        	    				    }
-        	    				};
-        					
-        					client.update(document).then(function(resp) {
-        						console.log('update'); 
-        				     });
-    					}, 1000);
-    					myTimer.invoke();
-    					clearInterval(myTimer);
-    					
+    					pushData(filedata[i]);
         			}
     			}catch(e){
     				logger.error(e);
@@ -98,11 +43,69 @@ var io = schedule.scheduleJob('0 30 3 * * *', function(){
 	});
 	
 	logger.info("migration_End");
-}); 
-
-function getData(){
-	io.invoke();
 }
+
+function sleep(ms){
+  return new Promise( resolve => {
+    setTimeout(resolve, ms);
+  })
+}
+var ms = 0;
+async function pushData( filedata ){
+    ms += 1000;
+    await sleep(ms);
+	
+	var document = {
+		index : 'call_'+filedata.ifId.slice(0,6),
+		type : "doc",
+		id : filedata.ifId.replace('_','-'),
+		body : { 
+			doc : {
+				  vdn : filedata.vdn,
+				  vdnGrp : filedata.vdnGrp,
+				  vdnNm : filedata.vdnNm,
+				  vdnGrpNm : filedata.vdnGrpNm,
+				  inCate : filedata.inCate,
+				  inCateNm : filedata.inCateNm,
+				  category1 : filedata.category1,
+				  category1Nm : filedata.category1Nm,
+				  category2 : filedata.category2,
+				  category2Nm : filedata.category2Nm,
+				  productCode : filedata.productCode,
+				  productNm : filedata.productNm,
+				  Mcate : filedata.Mcate,
+				  McateNm : filedata.McateNm,
+				  company : filedata.company,
+				  companyNm : filedata.companyNm,
+				  mdId : filedata.mdId,
+				  mdNm : filedata.mdNm,
+				  PCate : filedata.PCate,
+				  PCateNm : filedata.PCateNm,
+				  age : filedata.age,
+				  gender : filedata.gender,
+				  reasonCate1 : filedata.reasonCate1,
+				  reasonCate1Nm : filedata.reasonCate1Nm,
+				  reasonCate2 : filedata.reasonCate2,
+				  reasonCate2Nm : filedata.reasonCate2Nm,
+				  caseId : filedata.caseId,
+				  caseNumber : filedata.caseNumber,
+				  customerNumber : filedata.customerNumber,
+				  status : filedata.status,
+				  reasonDescription : filedata.reasonDescription 
+			},
+		doc_as_upsert: true
+		}
+	};
+
+	client.update(document).then(function(resp) {
+		console.log('update');
+		logger.info("update success : " + document.id); 
+	}, function(err){
+			logger.error("update failed : " + document.id);
+	});
+
+}
+
 getData();
 
 module.exports = getData;
