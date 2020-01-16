2577ee31d074b0255a26a6c1bec342fb1666c7bb
FDC00073 <FDC00073@IC0-W-N09161R.ssg.global>
2020-01-16 09:30:30 +0900

    정렬 필드값 수정

--- routes/voc.js
+++ routes/voc.js
@@ -67,7 +67,7 @@ router.post("/search", function(req, res){
 	}
 	
 	body.sort = {
-        "start_time" : "desc" // 파라미터로 받아서 정렬하기 
+        "startTime" : "desc" // 파라미터로 받아서 정렬하기 
     }
         
     client.search({
