f4ffb2e725b1411428fc2785c1b0f2e41c431138
kiyeop <kiyeop@gmail.com>
2020-01-28 11:26:47 +0900

    Update if_dma_00004.js
    대유 토큰 가져오는 것 수정.

--- if_dma_00004.js
+++ if_dma_00004.js
@@ -79,106 +79,106 @@ var options2 = {
 */
 
 function getData(){
-	rp(options1)
-    .then(function (body) {
-    	var token = JSON.parse(body);
-    	
-    	var path = config.send_save_path;
-        var sj = schedule.scheduleJob('30 * * * * *', function(){
-    	    var send_data = {};
-    	    send_data.param = [];
-    	    !fs.existsSync(config.sent_save_path) && fs.mkdirSync(config.sent_save_path);
-    	    !fs.existsSync(config.sent_smry_path) && fs.mkdirSync(config.sent_smry_path);
-    	    var z = 0;
-    	    pool.getConnection(function(err, connection){
-    	    	fs.readdir(config.send_smry_path, function(err, filelist){
-    	        	filelist.forEach(function(file) {
-    	        		fs.readFile(config.send_smry_path+file , 'utf-8' , function(err , filedata){
-    	        			if(err) { return callerror(err); }
-    	        			try{
-    	        				filedata = JSON.parse(filedata);
-    		        			var querystring  = "update ua_call set category1 = lpad(?, 10, '0'), summary = ? where start_time = ? and extension = ?";
-    		        			
-    		        			var callSQLquery = connection.query(querystring, [ filedata.category, filedata.summary, filedata.startTime, filedata.extension ], function (err, rows) {
-    		        				if(err){
-    						        	connection.release();
-    						            throw err;
-    						        }else{
-    						        	logger.info("if_dma_00004_Db_Query_callSQLquery ifID: " + filedata.startTime + "-" + filedata.agentId);
-    								}
-    						        connection.commit(function(err){
-    						            if(err){
-    						                connection.rollback(function(err){
-    						                    throw err;
-    						                });
-    						            } else {
-    						                console.log("Updated successfully!");
-    						            }
-    						        });
-    						        //z = parseInt(z)+1;
-    						        //send_data.sendTime = dateFormat(new Date(), "yyyymmddHHMMss");
-    					        	fs.rename(config.send_smry_path+file, config.sent_smry_path+file, callback);
-    						    });
-    	        			}catch(e){
-    	        				logger.error(e);
-    	        			}
-    					});
-    	    		});
-    	    		
-    	    	});
-    			connection.release();
-    	    });
-        });  
-        sj.invoke();
-        
-        var sj2 = schedule.scheduleJob('20 * * * * *', function(){
-    		!fs.existsSync(config.sent_save_path) && fs.mkdirSync(config.sent_save_path);
-        	!fs.existsSync(config.send_error_path) && fs.mkdirSync(config.send_error_path);
-            var z = 0;
-            fs.readdir(config.send_save_path, function(err, filelist){
-            	filelist.forEach(function(file) {
-            		fs.readFile(config.send_save_path+file , 'utf-8' , function(err , filedata){
-            			if(err) { return callerror(err); }
-            			var send_data = {};
-            	        send_data.param = [];
-            	        try{
-            	        	filedata = JSON.parse(filedata);
-            	        
-    	        			send_data.param.push(filedata);
-    	        			send_data.sendTime = dateFormat(new Date(), "yyyymmddHHMMss");
-    	        			
-    	        			options2.headers.Authorization = "OAuth " + token.access_token;
-            	            req_body = JSON.stringify(send_data);
-                	        console.log(req_body);
-            	            options2.body = req_body;
-            	            
-            	            rp(options2).then(function ( data ){
-            	            	z = parseInt(z)+1;
-            	            	data = JSON.parse(data);
-                    	        if(data.code == "10"){
-            	            		fs.rename(config.send_save_path+file, config.sent_save_path+file, callback);
-    								logger.info("if_dma_00004 file send success ifID: " + filedata.startTime + "-" + filedata.agentId);
-            	            	}else if(data.code == "99"){
-            	            		fs.rename(config.send_save_path+file, config.send_error_path+file, callback);
-    								logger.info("if_dma_00004 file send failed ifID: " + + filedata.startTime + "-" + filedata.agentId);
-            	            	}
-            	            }).catch(function (err){
-            	            	logger.error(err);
-            	            });
-            	        }catch(e){
-            	        	logger.error(e);
-            	        }
-            		});
-    			});
-        	});
-            
-        });  
-        sj2.invoke();
-    })
-    .catch(function (err) {
-    	logger.error(err);
-    });
 	
+	var path = config.send_save_path;
+
+	var sj = schedule.scheduleJob('30 * * * * *', function(){
+		var send_data = {};
+		send_data.param = [];
+		!fs.existsSync(config.sent_save_path) && fs.mkdirSync(config.sent_save_path);
+		!fs.existsSync(config.sent_smry_path) && fs.mkdirSync(config.sent_smry_path);
+		var z = 0;
+		pool.getConnection(function(err, connection){
+			fs.readdir(config.send_smry_path, function(err, filelist){
+				filelist.forEach(function(file) {
+					fs.readFile(config.send_smry_path+file , 'utf-8' , function(err , filedata){
+						if(err) { return callerror(err); }
+						try{
+							filedata = JSON.parse(filedata);
+							var querystring  = "update ua_call set category1 = lpad(?, 10, '0'), summary = ? where start_time = ? and extension = ?";
+							
+							var callSQLquery = connection.query(querystring, [ filedata.category, filedata.summary, filedata.startTime, filedata.extension ], function (err, rows) {
+								if(err){
+									connection.release();
+									throw err;
+								}else{
+									logger.info("if_dma_00004_Db_Query_callSQLquery ifID: " + filedata.startTime + "-" + filedata.agentId);
+								}
+								connection.commit(function(err){
+									if(err){
+										connection.rollback(function(err){
+											throw err;
+										});
+									} else {
+										console.log("Updated successfully!");
+									}
+								});
+								//z = parseInt(z)+1;
+								//send_data.sendTime = dateFormat(new Date(), "yyyymmddHHMMss");
+								fs.rename(config.send_smry_path+file, config.sent_smry_path+file, callback);
+							});
+						}catch(e){
+							logger.error(e);
+						}
+					});
+				});
+				
+			});
+			connection.release();
+		});
+	});  
+	sj.invoke();
+    
+	var sj2 = schedule.scheduleJob('20 * * * * *', function(){
+		!fs.existsSync(config.sent_save_path) && fs.mkdirSync(config.sent_save_path);
+		!fs.existsSync(config.send_error_path) && fs.mkdirSync(config.send_error_path);
+		
+		rp(options1).then(function (body) {
+			var token = JSON.parse(body);
+			var z = 0;
+			fs.readdir(config.send_save_path, function(err, filelist){
+				filelist.forEach(function(file) {
+					fs.readFile(config.send_save_path+file , 'utf-8' , function(err , filedata){
+						if(err) { return callerror(err); }
+						var send_data = {};
+						send_data.param = [];
+						try{
+							filedata = JSON.parse(filedata);
+						
+							send_data.param.push(filedata);
+							send_data.sendTime = dateFormat(new Date(), "yyyymmddHHMMss");
+							
+							options2.headers.Authorization = "OAuth " + token.access_token;
+							req_body = JSON.stringify(send_data);
+							console.log(req_body);
+							options2.body = req_body;
+							
+							rp(options2).then(function ( data ){
+								z = parseInt(z)+1;
+								data = JSON.parse(data);
+								if(data.code == "10"){
+									fs.rename(config.send_save_path+file, config.sent_save_path+file, callback);
+									logger.info("if_dma_00004 file send success ifID: " + filedata.startTime + "-" + filedata.agentId);
+								}else if(data.code == "99"){
+									fs.rename(config.send_save_path+file, config.send_error_path+file, callback);
+									logger.info("if_dma_00004 file send failed ifID: " + + filedata.startTime + "-" + filedata.agentId);
+								}
+							}).catch(function (err){
+								logger.error(err);
+							});
+						}catch(e){
+							logger.error(e);
+						}
+					});
+				});
+			});
+		
+		}).catch(function (err) {
+			logger.error(err);
+		});
+		
+	});  
+	sj2.invoke();
 	
 }
 
